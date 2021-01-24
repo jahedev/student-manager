5afa6b27ab4cb0246fe9f7c96c96a0122c664bf3
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-24 15:28:35 -0500

    navbar on app.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Navbar from './Navbar'
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import Navbar from "./Navbar"
 
 class Homepage extends Component {
   render() {
     return (
-      <div className='container'>
-        <Navbar />
-        <div className='bg-whole'></div>
+      <div className="container">
+        {/* <Navbar /> */}
+        {/* <h1>homepage</h1> */}
+        <div className="bg-whole"></div>
       </div>
     )
   }
