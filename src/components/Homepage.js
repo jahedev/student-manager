a2e98011f282b7659cab166f75dc6567ffc0fb19
jahedev <jahedev@gmail.com>
2021-01-24 18:32:07 -0500

    minor changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,21 @@
-import React, { Component } from "react"
-import { Link } from "react-router-dom"
-import Navbar from "./Navbar"
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from './Navbar'
+import '../css/homepage.css'
 
 class Homepage extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="bg-whole"></div> {/* <- bkg img on homepage */}
+      <div className='container'>
+        <div className='bg-whole'>
+          <div className='middle-align'>
+            <h1 className='homepage-header'>CRUD APPLICATION</h1>
+            <h3 className='homepage-subtitle'>
+              Team: Karim Nekzad, Marin Kocollari, M. Jahed Hossain, Hassan
+              Akbar
+            </h3>
+          </div>
+        </div>
       </div>
     )
   }
