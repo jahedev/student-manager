9e727ac531802c518aae9a60f98a25414896ac92
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-24 21:31:37 -0500

    fix all campuses view

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -8,9 +8,13 @@ class Campus extends Component {
   render() {
     const { id, campusname, image, address, description, Students } = this.props
     return (
-      <div>
-        <Link to={`/singleCampus/${id}`}>Campus Name: {campusname}</Link>
-        <p>Image: {image}</p>
+      <div className="campus">
+        <Link to={`/singleCampus/${id}`} className="campusNames">
+          {campusname}
+          <p className="description">{description}</p>
+        </Link>
+        <img src={image} className="campusImages" />
+        <div className="totalStudents">Students: {Students.length}</div>
         {/* Requirements just want campus name and image displayed on all campuses */}
         {/* <p>Address: {address}</p> */}
         {/* <p>Description: {description}</p> */}
